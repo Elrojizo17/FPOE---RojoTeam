30df1ea94c4971084a6c0059bbf043f46e1078ec
Manuel <manue.torres@correounivalle.edu.co>
2024-06-03 14:28:12 -0500

    Backend Corregido

--- Trabajo Final Eventos/front/Vistas/vistas.py
+++ Trabajo Final Eventos/front/Vistas/vistas.py
@@ -32,17 +32,13 @@ class Vista():
 
         self.menuClientes = tkinter.Menu(self.menu)
         self.menu.add_cascade(label="Gestionar Clientes", menu=self.menuClientes)
-        self.menuClientes.add_command(label="Registrar Cliente", command=lambda: self.registrarCliente())
+        self.menuClientes.add_command(label="Gestión de Clientes", command=lambda: self.registrarCliente())
         self.menuClientes.add_separator()
-        self.menuClientes.add_command(label="Actualizar Cliente", command=lambda: self.actualizarCliente())
-        self.menuClientes.add_separator()
-        self.menuClientes.add_command(label="Borrar Cliente", command=lambda: self.borrarCliente())
-        self.menuClientes.add_separator()
-        self.menuClientes.add_command(label="Consultar Cliente", command=lambda: self.consultarCliente())
+        
 
         self.menuServicios = tkinter.Menu(self.menu)
         self.menu.add_cascade(label="Gestionar Servicios", menu=self.menuServicios)
-        self.menuServicios.add_command(label="Registrar Servicio", command=lambda: self.registrarServicio())
+        self.menuServicios.add_command(label="Gestión de Servicios", command=lambda: self.registrarServicio())
 
         self.menuSalir = tkinter.Menu(self.menu)
         self.menu.add_cascade(label="Salir", menu=self.menuSalir)
